7a606b339d3d097864e0b58c7ec3e967dc0a2873
jitender-axovel <jitender.axovel@gmail.com>
2017-09-04 14:37:41 +0530

    sidebar done

--- src/app/dashboard/dash-board.component.ts
+++ src/app/dashboard/dash-board.component.ts
@@ -1,12 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,  ViewChild,
+trigger, state, style, transition, animate} from '@angular/core';
 import { Router,ActivatedRoute } from '@angular/router';
+import {MdSidenav} from '@angular/material';
 
 @Component({
   moduleId:module.id,
   templateUrl: './dash-board.component.html',
-  styleUrls: ['./dash-board.component.css']
+  styleUrls: ['./dash-board.component.css'],
+  animations: [
+    trigger('contentOffset', [
+        state('add', style({
+            // 'margin-left': '20px',
+            // 'background':'red'
+            'transition':'transform .100s cubic-bezier(.25,.8,.25,1)'
+        })),
+        state('remove', style({
+            // 'margin-left': '0',
+            'width':'90px',
+            'visibility':'visible',
+            'transform':'translate3d(-26%,0,0)',
+            // 'background':'blue'
+        })),
+        transition('remove => add', animate('1ms ease'))
+    ]),
+    trigger('contentinerSet', [
+        state('addOpen', style({
+            'margin-left': '65px',
+            // 'background':'red'
+        })),
+        state('removeClose', style({
+            // 'margin-left': '0px',
+            // 'transform':'translate3d(0px,0px,0px)',
+            // 'background':'red'
+        })),
+        // transition('addOpen => removeClose', animate('1000ms ease'))
+    ]),
+]
 })
 export class DashBoardComponent implements OnInit {
+  @ViewChild('sidenav') sidenav:MdSidenav;
+  contentOffsetStatus:string = 'add';
+  contentinerSetStatus:string;
+  public Checksidebar:any=0;
   public user:any;
   public dashBoardCheck:any=1;
   public role ={
@@ -60,6 +95,20 @@ export class DashBoardComponent implements OnInit {
   }
   listEmp(){
         this.router.navigate(['dashboard/list']);
+  }
+  sidebarToggle(){
+    this.contentOffsetStatus = ( this.contentOffsetStatus === 'add' ? 'remove' : 'add' );
+    if(this.contentOffsetStatus == 'remove'){
+      this.contentinerSetStatus ='addOpen';
+      this.Checksidebar=1;
+    }
+    else{
+      this.Checksidebar=0;
+      // document.getElementsByClassName("mat-sidenav-content").style.display = "hidden";
+      this.contentinerSetStatus='removeClose'
+    }
+    this.sidenav.toggle();
+        // document.getElementsByClassName("mat-sidenav.mat-sidenav-closed").style.visibility="visible";
   }
 
 }
